c2a4e4a1e26b71e2c6ffcfc7723c78a67c82d9aa
weixian.zhou <weixian.zhou@ninebot.com>
2020-03-30 17:55:33 +0800

    fix install-vim error

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
+#!/bin/sh
+
+image_name=android-ci
+version=0.0.1
+container_name=jenkins
+host_port=8082
+
 docker pull jenkins/jenkins && 
-docker build -f dockerfile-jenkins-android -t android-package:0.0.1 .
+docker build -f dockerfile-jenkins-android -t $image_name:$version .
+
+
+docker run --name $container_name -di -p $host_port:8080 -v ~/docker/jenkins/:/var/jenkins_home -v $image_name:$version
