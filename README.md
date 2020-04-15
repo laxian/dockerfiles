02e1d3d67f1a53b8d323703045de21449fab65fc
zhouweixian <laxian2010@qq.com>
2020-04-15 14:02:28 +0800

    set timezone

--- README.md
+++ README.md
@@ -22,3 +22,31 @@ docker nginx image based on alpine
 file upload/download based on openresty
 
 [docker-openresty/README.md](https://github.com/laxian/dockerfiles/blob/master/docker-openresty/README.md)
+
+Docker 时区问题
+
+1. 启动时指定时区
+`docker run -e TZ=Asia/Shanghai nginx`
+
+2. 改变镜像或容器时区
+
+- alpine
+```shell
+ENV TZ=Asia/Shanghai
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
+    # timezone
+    && apk update && apk add ca-certificates \
+    && apk add tzdata \
+    && ln -sf /usr/share/zoneinfo/$TZ /etc/localtime \
+    && echo $TZ > /etc/timezone \
+```
+
+- Ubuntu
+```shell
+ENV TZ=Asia/Shanghai
+
+# timezone
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone \
+    && apt-get install tzdata \
+    && dpkg-reconfigure --frontend noninteractive tzdata
+```
--- nginx/onekey_nginx.sh
+++ nginx/onekey_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# docker pull nginx
-docker run --name nginx -d -p 80:80 -v ~/docker/share:/share nginx
+# docker pull nginx，setup timezone "-e TZ=Asia/Shanghai"
+docker run --name nginx -d -p 80:80 -v ~/docker/share:/share -e TZ=Asia/Shanghai nginx
 
 # 查询新安装的container的id
 containerId=$(docker ps -l | tail -n 1 | cut -d' ' -f1)
@@ -13,16 +13,23 @@ echo $containerId
 # 复制回去到docker nginx
 docker cp default.conf $containerId:/etc/nginx/conf.d/
 
+# 修改时区
+# TZ=Asia/Shanghai
+# docker exec -it nginx /bin/sh -cx "ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone \
+#     && apt-get install tzdata \
+#     && cat /etc/timezone"
+# docker exec -it nginx /bin/sh -cx "dpkg-reconfigure --frontend noninteractive tzdata"
+
 # in docker 执行nginx -s reload
 docker exec -it nginx /bin/sh -c "nginx -s reload"
 
-if [ ! -d "~/docker/share/" ];then
+if [ ! -d "~/docker/share/" ]; then
     mkdir -p ~/docker/share
 fi
 
 if [ ! -f ~/docker/share/a.txt ]; then
     timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-    echo hello world! $timestamp > ~/docker/share/a.txt
+    echo hello world! $timestamp >~/docker/share/a.txt
 fi
 
 # 打开
